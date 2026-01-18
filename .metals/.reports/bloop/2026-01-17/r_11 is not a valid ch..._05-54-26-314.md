fab4459905269d1c077953a2a7d2c0930c2f7dfa
Fsehaye Medhanie <werkat41@gmail.com>
2026-01-18 01:05:41 -0800

    update

---
+++ .metals/.reports/bloop/2026-01-17/r_11 is not a valid ch..._05-54-26-314.md
@@ -0,0 +1,7 @@
+error id: m4LWmXMnJAlYKMWgi4p1eA==
+### Bloop error:
+
+11 is not a valid choice for -java-output-version
+#### Short summary: 
+
+11 is not a valid choice for -java-output-version
---
+++ Coursera_course/scala-project/src/main/scala/example/.scala-build/example_7a511de1dc-6170e68dbe/src_generated/main/tempCodeRunnerFile.scala
@@ -0,0 +1,34 @@
+
+
+final class tempCodeRunnerFile$_ {
+def args = tempCodeRunnerFile_sc.args$
+def scriptPath = """d:\SFBU\Spring_semester_2026\CS570\Coursera_course\scala-project\src\main\scala\example\tempCodeRunnerFile.sc"""
+/*<script>*/
+1 + 1
+
+val x = 42
+
+x * x
+/*</script>*/ /*<generated>*//*</generated>*/
+}
+
+object tempCodeRunnerFile_sc {
+  private var args$opt0 = Option.empty[Array[String]]
+  def args$set(args: Array[String]): Unit = {
+    args$opt0 = Some(args)
+  }
+  def args$opt: Option[Array[String]] = args$opt0
+  def args$: Array[String] = args$opt.getOrElse {
+    sys.error("No arguments passed to this script")
+  }
+
+  lazy val script = new tempCodeRunnerFile$_
+
+  def main(args: Array[String]): Unit = {
+    args$set(args)
+    val _ = script.hashCode() // hashCode to clear scalac warning about pure expression in statement position
+  }
+}
+
+export tempCodeRunnerFile_sc.script as `tempCodeRunnerFile`
+
